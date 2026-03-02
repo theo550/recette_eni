5827c1447f55a15e11bd7a42deb62a31a22e40ff
theo550 <theo.gaudin35@gmail.com>
2026-03-02 12:28:43 +0100

    add reciep model

--- ReciepApp/admin.py
+++ ReciepApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from ReciepApp.models import Reciep
+
 # Register your models here.
+admin.site.register(Reciep)
--- ReciepApp/models.py
+++ ReciepApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Reciep(models.Model):
+    title = models.CharField(max_length=30)
+    category = models.CharField(max_length=40)
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.title
